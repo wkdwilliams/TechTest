8adaed8490da5ddcbcff0c4b6f877d1af7b18541
wkdwilliams <31390496+wkdwilliams@users.noreply.github.com>
2021-04-14 00:25:20 +0700

    Update migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,20 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('mobile');
+            $table->string('birthday_day');
+            $table->string('birthday_month');
+            $table->string('birthday_year');
+            $table->string('spacify');
+            $table->string('member_card');
+            $table->string('expiration_day');
+            $table->string('expiration_month');
+            $table->string('expiration_year');
+            $table->text('address');
             $table->timestamps();
         });
     }
